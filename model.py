74eab6d2e95e6038d80b538d8cfb2115b3f5af4b
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-10 15:02:20 +0200

    Added TimeSession class and mapping. Added a query for testing.

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
-import sqlalchemy
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
-engine = sqlalchemy.create_engine('sqlite:///.db/test.db', echo=True)
+import sqlalchemy
+import datetime
+
+from sqlalchemy.orm import mapper, sessionmaker
+
+engine = sqlalchemy.create_engine('sqlite:///.db/test.db', encoding='utf8', echo=True)
 metadata = sqlalchemy.MetaData()
-session_table = sqlalchemy.Table('sessions', metadata,
+timesession_table = sqlalchemy.Table('sessions', metadata,
                                  sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
                                  sqlalchemy.Column('start_time', sqlalchemy.DateTime),
                                  sqlalchemy.Column('end_time', sqlalchemy.DateTime),
@@ -14,4 +20,33 @@ tags_table = sqlalchemy.Table('tags', metadata,
                                  sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
                                  sqlalchemy.Column('name', sqlalchemy.String)
                                  )
+
 metadata.create_all(engine)
+
+class TimeSession(object):
+    def __init__(self, start_time, end_time, unit, description, tags):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.unit = unit
+        self.description = description
+        self.tags = tags
+            
+    def __repr__(self):
+        return "<TimeSession('%s','%s','%s','%s','%s')>" % (self.start_time, self.end_time, self.unit, self.description, self.tags)
+    
+mapper(TimeSession, timesession_table)
+
+new_timesession = TimeSession(datetime.datetime.now(), datetime.datetime.now()+datetime.timedelta(days=1), 1, u"Leírás", u"cimkék")
+
+SessionMaker = sessionmaker(bind=engine)
+
+session = SessionMaker()
+
+session.add(new_timesession)
+
+new_timesession = TimeSession(datetime.datetime.now(), datetime.datetime.now()+datetime.timedelta(days=1), 1, u"Leírás2", u"cimkék")
+session.add(new_timesession)
+
+our_timesession = session.query(TimeSession).filter_by(tags=u'cimkék').all()
+
+print(our_timesession)
